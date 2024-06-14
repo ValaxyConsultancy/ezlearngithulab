e3624ba6ac4ce8a430eca29fb020823ff6e13933
admin <admin@gmail.com>
2024-06-13 21:46:05 -0500

    updates

--- src/main/java/com/ezlearn/metrics/MetricsContextListener.java
+++ src/main/java/com/ezlearn/metrics/MetricsContextListener.java
@@ -9,10 +9,10 @@ import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.core.instrument.binder.tomcat.TomcatMetrics;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusConfig;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import org.apache.catalina.Manager;
 
 @WebListener
 public class MetricsContextListener implements ServletContextListener {
@@ -31,7 +31,10 @@ public class MetricsContextListener implements ServletContextListener {
         new LogbackMetrics().bindTo(prometheusRegistry);
         new ProcessorMetrics().bindTo(prometheusRegistry);
         new UptimeMetrics().bindTo(prometheusRegistry);
-        new TomcatMetrics(TomcatMetrics.getTomcatManager(sce.getServletContext())).bindTo(prometheusRegistry);
+
+        // Assuming you have a method to get the Manager instance
+        Manager manager = getManagerFromContext(sce.getServletContext());
+        new TomcatMetrics(manager, null).bindTo(prometheusRegistry);
 
         sce.getServletContext().setAttribute("prometheusRegistry", prometheusRegistry);
         sce.getServletContext().addServlet("metricsServlet", new MetricsServlet(prometheusRegistry)).addMapping("/metrics");
@@ -40,5 +43,9 @@ public class MetricsContextListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         // Clean up if necessary
+    }
+
+    private Manager getManagerFromContext(javax.servlet.ServletContext context) {
+        return (Manager) context.getAttribute("org.apache.catalina.Manager");
     }
 }
