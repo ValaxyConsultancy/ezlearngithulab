74f054f0b7f33aa8d401c34befb8290276caa8a3
admin <admin@gmail.com>
2024-06-13 19:12:15 -0500

    updates

--- src/main/java/com/ezlearn/metrics/MetricsContextListener.java
+++ src/main/java/com/ezlearn/metrics/MetricsContextListener.java
@@ -7,10 +7,15 @@ import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.core.instrument.binder.tomcat.TomcatMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import org.apache.catalina.Manager;
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
+import java.util.Collections;
 
 @WebListener
 public class MetricsContextListener implements ServletContextListener {
@@ -18,7 +23,7 @@ public class MetricsContextListener implements ServletContextListener {
     private final PrometheusMeterRegistry prometheusRegistry;
 
     public MetricsContextListener() {
-        this.prometheusRegistry = new PrometheusMeterRegistry(PrometheusMeterRegistry.DEFAULT);
+        this.prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
     }
 
     @Override
@@ -29,7 +34,12 @@ public class MetricsContextListener implements ServletContextListener {
         new LogbackMetrics().bindTo(prometheusRegistry);
         new ProcessorMetrics().bindTo(prometheusRegistry);
         new UptimeMetrics().bindTo(prometheusRegistry);
-        new TomcatMetrics().bindTo(prometheusRegistry);
+
+        // Get the Manager instance from your Tomcat context
+        Manager manager = ...; // Obtain the Manager instance from your application context
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        new TomcatMetrics(manager, Collections.emptyList(), mBeanServer).bindTo(prometheusRegistry);
+
         sce.getServletContext().setAttribute("prometheusRegistry", prometheusRegistry);
     }
 
